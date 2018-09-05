87c25c6475779a6abe4d7e705d3fe2a36ccf93fe
ssj5638 <ssj5638@gmail.com>
2018-09-05 22:59:29 +0900

    크롤링한 xml형식 데이터를 Dict형식으로 바꿔 key를 column으로 잡음

--- collection/crawling.py
+++ collection/crawling.py
@@ -4,18 +4,63 @@ import sys
 from urllib.request import Request, urlopen
 from datetime import datetime
 import math
-import lxml
+import json
 import pandas as pd
 import os
+import xmltodict
 
-RESULT_DIRECTORY = '../__result__/crawling'
+RESULT_DIRECTORY = '__result__/crawling'
 
 #동물관리보호소  공공데이터 API 서비스키
 SURVICE_KEY = 'kjzgwf40zX1dWlcX1PKEw2r%2BUfP1YbASnaMYa5dQ6aOr9mFJOi%2FcDQB%2FRlvaWdCVVQ5uSS%2BWwXU1WJYDAFmmBA%3D%3D'    # 김민규
 # SURVICE_KEY = 'EdieVeGWBCgcq7f02Z4gpx%2FEssqE8l151SGr%2FHYps1SvWYKgXvpn35kSxTQUhMkxyf9yOrp2SU%2Fr9xZjf7aWQA%3D%3D'    # 송수진
 # SURVICE_KEY = 'FXCllNPKHcV69y%2FBSMoNxmHKoCNvPRagdDMlScSRSsNJLbGqp12VchucwYzzGf1jWKNrbyi%2BBDF0zJSL%2Bi4K3Q%3D%3D'  # 김신애
 base_url='http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic'
-# bgnde=20180101&endde=20180901&pageNo=1&numOfRows=10&ServiceKey=
+
+
+def animal_url(base=base_url, **params):
+    url = '%s?%s&ServiceKey=%s' % (base, urlencode(params), SURVICE_KEY)
+    return url
+
+def animal_crawling():
+    for year in range(2008, 2019):  # 년도가 바뀔 때마다, pageNo, isnext 초기화
+        bgnde = '%s0101' % (year)
+        endde = '%s1231' % (year)
+        pageNo = 1
+        isnext = True
+        encoding = 'utf-8'
+
+        while isnext:
+            url = animal_url(bgnde=bgnde, endde=endde, pageNo=pageNo, numOfRows=1000)
+            res = urlopen(url=url)
+
+            if res is None:
+                break
+
+            xml_result = res.read().decode(encoding)
+
+            xml_data = xmltodict.parse(xml_result)
+            xml_dict = json.dumps(xml_data)
+            dict_result = json.loads(xml_dict)
+            xml_body = dict_result.get('response').get('body')
+
+            xml_nor = None if dict_result is None else xml_body.get('numOfRows')
+            xml_tc = None if dict_result is None else xml_body.get('totalCount')
+
+            lostData = xml_body.get('items').get('item')
+
+            cnt = math.ceil(int(xml_tc)/int(xml_nor))   # 크롤링 횟수 확인
+
+            if pageNo == cnt:
+                isnext = False
+            else:
+                pageNo += 1
+
+            df = pd.DataFrame(lostData)     # Dict -> DataFrame
+            print(df)
+
+        filename = '{0}/lostAnimal_{1}_{2}.csv'.format(RESULT_DIRECTORY,bgnde,endde)
+        df.to_csv(filename, mode='w', encoding='euc-kr', index=True)
 
 
 def crawler(
@@ -39,55 +84,6 @@ def crawler(
 
     except Exception as e:
         err(e)
-
-
-def animal_url(base=base_url, **params):
-    url = '%s?%s&ServiceKey=%s' % (base, urlencode(params), SURVICE_KEY)
-    return url
-
-def animal_crawling():
-    for year in range(2017, 2018):
-        bgnde = '%s0101' % (year)
-        endde = '%s0101' % (year)
-        pageNo = 1
-        isnext = True
-        datalist = []
-
-        while isnext:
-            url = animal_url(bgnde=bgnde, endde=endde, pageNo=pageNo, numOfRows=1000)
-            res = urlopen(url=url)
-
-            if res is None:
-                break
-
-            xml_result = BeautifulSoup(res, 'xml')
-
-            xml_body = xml_result.find('body')
-            xml_nor = None if xml_result is None else xml_body.find('numOfRows')
-            xml_tc = None if xml_result is None else xml_body.find('totalCount')
-
-            lostData = xml_body.findAll('item')
-
-            for data in list(lostData):
-                c = list(data.strings)
-                datalist.append(c)
-
-            nor = list(xml_nor.strings)
-            tc = list(xml_tc.strings)
-            print(tc)
-
-            cnt = math.ceil(int(tc[0])/int(nor[0]))
-
-            if pageNo == cnt:
-                isnext = False
-            else:
-                pageNo += 1
-
-            df = pd.DataFrame(datalist)
-            print(df)
-
-        df.to_csv('{0}/lostAnimal_{1}_{2}.csv'.format(RESULT_DIRECTORY,bgnde,endde), encoding='utf-8', mode='w', index=True)
-
 
 
 def shelter_crawling():
@@ -122,13 +118,10 @@ def shelter_crawling():
 
     table = pd.DataFrame(results, columns=['area','name','number','address'])
     #print(table)
-    #table.to_csv('shelter.csv', encoding='utf-8', mode='w',index=True)
-    table.to_csv('{0}/shelter.csv'.format(RESULT_DIRECTORY),encoding='utf-8',mode='w',index=True)
-
-
-
+    table.to_csv('shelter.csv', encoding='utf-8', mode='w',index=True)
+    #table.to_csv('{0}/shelter.csv'.format(RESULT_DIRECTORY),encoding='utf-8',mode='w',index=True)
 
 
 if __name__ == '__main__':
-    #shelter_crawling()
+    # shelter_crawling()
     animal_crawling()
